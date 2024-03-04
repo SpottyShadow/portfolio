6fbfeca6c694071b10443434d4fecdee4b9d23df
SpottyShadow <89530648+SpottyShadow@users.noreply.github.com>
2024-03-04 21:17:43 +0100

    aa

--- src/data/sections/main-section.data.ts
+++ src/data/sections/main-section.data.ts
@@ -15,7 +15,7 @@ const mainSectionData = {
   details: [
     { label: 'Téléphone', value: '06 43 21 56 09', url: 'tel:06 43 21 56 09' },
     { label: 'Email', value: 'lemoinetheo2003@gmail.com', url: 'mailto:lemoinetheo2003@gmail.com' },
-    { label: 'From', value: 'Meaux, France' },
+    { label: 'Localisation', value: 'Toulouse, France' },
     //{ label: 'Salary range', value: '18 000 - 25 000 PLN' },
   ],
   pdfDetails: [
@@ -29,9 +29,9 @@ const mainSectionData = {
     'Lorem ipsum dolor sit amet, consectetur **adipiscing elit**. In sodales ac dui at *vestibulum*. In condimentum metus id dui tincidunt, in blandit mi [vehicula](/). Nulla lacinia, erat sit amet elementum vulputate, lectus mauris volutpat mi, vitae accumsan metus elit ut nunc. Vestibulum lacinia enim eget eros fermentum scelerisque. Proin augue leo, posuere ut imperdiet vitae, fermentum eu ipsum. Sed sed neque sagittis, posuere urna nec, commodo leo. Pellentesque posuere justo vitae massa volutpat maximus.',
   tags: [{ name: 'Disponible à Alternance' }, { name: 'Disponible aux stages' }, { name: 'Travail sur des projets personnel' }],
   action: {
-    label: 'Download CV',
+    label: 'Télécharger le CV',
     url: '/cv.pdf',
-    downloadedFileName: 'CV-Mark_Freeman.pdf',
+    downloadedFileName: 'CV-Théo_Lemoine.pdf',
   },
   links: [facebook({ url: '#' }), github({ url: '#' }), linkedin({ url: '#' }), twitter({ url: '#' })],
 } as const satisfies ReadonlyDeep<MainSection>;
